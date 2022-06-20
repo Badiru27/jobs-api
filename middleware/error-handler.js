10e5f18f51dd3abe9c885e89a10727a574615e93
Badiru <55401789+Badiru27@users.noreply.github.com>
2022-06-20 17:44:42 +0100

    Implement error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,29 @@
-const { CustomApiError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomApiError) return res.status(err.statusCode).json({ msg: err.message });
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+    const customError = {
+        statusCode: err.status || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'It\'s not your fault. The error is from the server'
+    }
+
+    if (err.name === 'ValidationError') {
+        console.log(Object.values(err.errors))
+        customError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+        customError.statusCode= 400;
+    }
+
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate key entered for ${Object.values(err.keyValue)} field, please choose another value`;
+        customError.statusCode = 400;
+    }
+    if (err.name === 'CastError') {
+        customError.msg = `No item found with id: ${err.value}`;
+        customError.statusCode = 404;
+    }
+
+    return res.status(customError.statusCode).json({ msg: customError.msg });
 }
  
 module.exports = errorHandlerMiddleware
