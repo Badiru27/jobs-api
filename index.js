ed9fab6a10b5da6a9aa251f1dfea48e765bdd240
Badiru <55401789+Badiru27@users.noreply.github.com>
2022-06-20 18:08:33 +0100

    added middleware packages

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+
 const app = express();
 
 
 //connectDB
 const connectDB = require('./db/connect');
+
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const authenticateUser = require('.//middleware/authentication')
 //routes
@@ -16,7 +24,18 @@ const authRoute = require('./routes/auth');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
+
+app.set('trust proxy', 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 
 
@@ -26,6 +45,9 @@ app.use('/api/v1/job', authenticateUser, jobRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
+
+
+
 
 const port = process.env.PORT || 3000;
 const start = async () => {
