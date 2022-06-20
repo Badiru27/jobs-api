a9b387cac6400cbb5826ab1654a86f21fae2d21e
Badiru <55401789+Badiru27@users.noreply.github.com>
2022-06-20 20:54:20 +0100

    push to heroku

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ app.use(cors());
 app.use(xss());
 
 
-
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/job', authenticateUser, jobRoute);
 
